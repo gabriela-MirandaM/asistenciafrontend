8965e7169ef1d00679fe95eab1fd0a8dfa91f7e6
Gabriela <mm15136@ues.edu.sv>
2026-04-12 17:59:59 -0600

    cambios a variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  API_URL: `http://localhost:8000`,
+  API_URL: `https://despega.conexionelsalvador.org/backend`,
+  ACCESS_TOKEN_SECRET: "sn1v0VO6qUVhhbx3PUEusVystCzQSIId",
+  REFRESH_TOKEN_SECRET: "7tclclesecqROeFv0TplnZqZ4xW9vEUq"
+  // API_URL: `http://localhost:8000`,
+  // ACCESS_TOKEN_SECRET:"sn1v0VO6qUVhhbx3PUEusVystCzQSIId",
+  // REFRESH_TOKEN_SECRET:"7tclclesecqROeFv0TplnZqZ4xW9vEUq"
 };
