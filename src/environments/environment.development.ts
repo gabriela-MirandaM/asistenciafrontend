5c11f749d31a45ca3ae271841c1d5db59c487310
Gabriela <mm15136@ues.edu.sv>
2026-04-12 18:14:17 -0600

    Cambios a variables de entorno en frontend.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
   // ACCESS_TOKEN_SECRET:"sn1v0VO6qUVhhbx3PUEusVystCzQSIId",
   // REFRESH_TOKEN_SECRET:"7tclclesecqROeFv0TplnZqZ4xW9vEUq"
   API_URL: `http://localhost:8000`,
-  ACCESS_TOKEN_SECRET:"sn1v0VO6qUVhhbx3PUEusVystCzQSIId",
-  REFRESH_TOKEN_SECRET:"7tclclesecqROeFv0TplnZqZ4xW9vEUq"
+  ACCESS_TOKEN_SECRET: "sn1v0VO6qUVhhbx3PUEusVystCzQSIId",
+  REFRESH_TOKEN_SECRET: "7tclclesecqROeFv0TplnZqZ4xW9vEUq"
 };
